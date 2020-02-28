5c9e9004757cbd52fd0782e8613b409c70a8fda1
Jonas Voetmann <48560462+JonasVoet@users.noreply.github.com>
2020-02-28 01:46:44 +0100

    all

---
+++ precache-manifest.a377e2b2b69677a338785c26dcee1708.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d421c9fd31e9d5b8fe61ca455da22f",
+    "url": "/LocalMeals-app/index.html"
+  },
+  {
+    "revision": "9c881d8029ff028ed2fa",
+    "url": "/LocalMeals-app/static/css/2.b1f3d402.chunk.css"
+  },
+  {
+    "revision": "59cd7b25f601d0e7b6cf",
+    "url": "/LocalMeals-app/static/css/main.a9b9eb7a.chunk.css"
+  },
+  {
+    "revision": "9c881d8029ff028ed2fa",
+    "url": "/LocalMeals-app/static/js/2.f5e49e93.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/LocalMeals-app/static/js/2.f5e49e93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59cd7b25f601d0e7b6cf",
+    "url": "/LocalMeals-app/static/js/main.4b880e1e.chunk.js"
+  },
+  {
+    "revision": "10af3e3b1d3539c3475b",
+    "url": "/LocalMeals-app/static/js/runtime-main.e691d3de.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/LocalMeals-app/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "c01b093ad1779017e254ad9ad245e10a",
+    "url": "/LocalMeals-app/static/media/bg.c01b093a.jpg"
+  },
+  {
+    "revision": "953dcdaa6e8bdd8e8895a617e8bfa4bc",
+    "url": "/LocalMeals-app/static/media/salmon.953dcdaa.png"
+  },
+  {
+    "revision": "c9eb09d80cb11b66679bc91b804ab595",
+    "url": "/LocalMeals-app/static/media/spoon.c9eb09d8.png"
+  }
+]);
